9bb3a529b3963b3b41b76b9ccdd8bed609cb583b
Andrés Martínez <autummata@gmail.com>
2024-10-12 19:44:34 -0600

    Fix: Styling

--- app/(tabs)/credits.tsx
+++ app/(tabs)/credits.tsx
@@ -1,13 +1,37 @@
 import { ThemedText } from "@/components/ThemedText";
-import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ThemedView } from "@/components/ThemedView";
+import { StyleSheet } from "react-native";
+
 export default function Credits() {
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center",
+      backgroundColor: "white",
+      padding: 20,
+    },
+    personalInfo: {
+      paddingTop: 20,
+      backgroundColor: "white", 
+      textAlign: "center",
+    },
+    personalData: { 
+      fontStyle: "italic",
+      paddingBottom: 10,
+    }
+  });
   return (
-    <ParallaxScrollView
-      headerImage={<div />}
-      headerBackgroundColor={{ light: "", dark: "" }}
-    >
-      <ThemedText type="subtitle"> Información </ThemedText>
-      <ThemedText> Andrés Daniel Martínez - A00227463 </ThemedText>
-    </ParallaxScrollView>
+    <ThemedView style={styles.container}>
+      <ThemedText type="title"> Información </ThemedText>
+      <ThemedText type="subtitle"> Prof. Arturo Javier Lopez Fausto</ThemedText>
+
+      <ThemedView style={styles.personalInfo}>
+        <ThemedText style = {styles.personalData}> Andrés Daniel Martínez - A00227463 </ThemedText>
+        <ThemedText>
+          {" "}
+          Integración de Seguridad Informática en Redes y Sistemas de Software{" "}
+        </ThemedText>
+      </ThemedView>
+    </ThemedView>
   );
 }
